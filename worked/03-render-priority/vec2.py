21f5413ffaecce16b1d8a326161f4db5d5d6c590
Frank Davies <fdavies93@gmail.com>
2022-11-18 07:53:47 +0800

    Wrote quick Vec2 class to simplify number operations

---
+++ worked/03-render-priority/vec2.py
@@ -0,0 +1,30 @@
+import numbers
+
+class Vec2():
+    
+    def __init__(self, x = 0, y = 0):
+        if not (isinstance(x, numbers.Number) and isinstance(y, numbers.Number)):
+            raise TypeError()
+
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        if not isinstance(other, Vec2):
+            raise TypeError()
+
+        return Vec2(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        if not isinstance(other, Vec2):
+            raise TypeError()
+
+        return Vec2(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other):
+        if not isinstance(other, numbers.Number):
+            # note multiplying by another vector is also nonsensical as dot product, cross product are different operations
+            # divide operation not implemented because it's mathematically invalid, pass 1 / scalar to mul if you want this function 
+            raise TypeError()
+
+        return Vec2(self.x * other, self.y * other)
