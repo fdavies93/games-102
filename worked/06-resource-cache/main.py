11a4e1150f70fd6d3a82a254dbc41f3f9be4aa0b
Frank Davies <fdavies93@gmail.com>
2022-12-08 07:51:36 +0800

    Added run levels switchable by number key

--- worked/06-resource-cache/main.py
+++ worked/06-resource-cache/main.py
@@ -1,14 +1,18 @@
-import pygame
+import pygame, time, sys
 pygame.init()
 
-size = width, height = (1280, 960)
+size = width, height = (640, 480) # 20 x 15 at 32 x 32 tiles
 black = (0, 0, 0)
 screen = pygame.display.set_mode(size)
+frame_time = 1.0 / 60.0
+do_render = True
 
 class TileManager:
     def __init__(self, meta_index):
         # includes only name and filename of index {"dungeon": "index"}
+        self.run_level = 0
         self.tile_sets = self.load_index(meta_index)
+        self.run_level = 1
 
     def load_index(self, index):
         index_out = {}
@@ -28,32 +32,135 @@ class TileManager:
                 # otherwise, store this data as a rect
                 r = [int(x) for x in split[1:5]]
                 self.tile_sets[name]["tile_data"][split[0]] = {"rect": pygame.Rect(r)}
+        self.run_level = 2
+        print(f"Loaded data from {name}")
     
-    def load_tileset_sprite(self, ts):
+    def load_tileset_sprite(self, ts, zoom_level = 1):
         # it's already loaded
         if "sprite" in self.tile_sets[ts]:
             return
         image_path = "./assets/" + self.tile_sets[ts]["texture"]
         tileset_sprite = pygame.image.load(image_path).convert_alpha()
+        tileset_sprite = pygame.transform.scale(tileset_sprite, (tileset_sprite.get_width() * zoom_level, tileset_sprite.get_height() * zoom_level))
         self.tile_sets[ts]["sprite"] = tileset_sprite
         for tile in self.tile_sets[ts]["tile_data"].values():
-            tile["surface"] = tileset_sprite.subsurface(tile["rect"])
+            r : pygame.Rect = tile["rect"]
+            scaled_rect = pygame.Rect(r.left * zoom_level, r.top * zoom_level, r.width * zoom_level, r.height * zoom_level)
+            tile["surface"] = tileset_sprite.subsurface(scaled_rect)
+        self.run_level = 3
+        print(f"Loaded sprites from {ts}")
 
 
-    def unload_tileset(self,name):
-        pass
+    def unload_tileset_sprite(self, name):
+        for tile in self.tile_sets[name]["tile_data"].values():
+            del tile["surface"]
+        del self.tile_sets[name]["sprite"]
+        print(f"Deleted sprites from {name}")
+        self.run_level = 2
 
+    def unload_tileset_data(self, name):
+        # just wipe everything
+        self.tile_sets[name]["tile_data"] = {}
+        print(f"Deleted data from {name}")
+        self.run_level = 1
 
 class Map:
-    def __init__(self, tileset):
-        self.tilemap = tileset
-        pass
+    def __init__(self, tileset, grid_scale):
+        self.tileset = tileset
+        self.tile_data = []
+        self.grid_scale = grid_scale
+
+    def load_data(self, map_file):
+        with open('./assets/' + map_file, "r") as f:
+            for line in f:
+                split = line.split()
+                if len(split) != 5:
+                    continue
+                self.tile_data.append({"tile": split[0], "pos": (int(split[1]), int(split[2])), "size": (int(split[3]), int(split[4])) })
+
+    def render(self, tile_manager):
+        for r in self.tile_data:
+            start_x = r["pos"][0] * self.grid_scale[0]
+            start_y = r["pos"][1] * self.grid_scale[1]
+            width = r["size"][0]
+            height = r["size"][1]
+            sprite = tile_manager.tile_sets[self.tileset]["tile_data"][r["tile"]]["surface"]
+            for x in range(width):
+                for y in range(height):
+                    screen.blit(sprite, (start_x + (x * self.grid_scale[0]), start_y + (y * self.grid_scale[1])))
+
+def process_input(manager : TileManager):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            for tileset in manager.tile_sets:
+                manager.unload_tileset_sprite(tileset)
+                manager.unload_tileset_data(tileset)
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            key_name = pygame.key.name(event.key)
+            if key_name not in ["1", "2", "3"]:
+                return
+
+            unload_sprites = False
+            unload_data = False
+            load_sprites = False
+            load_data = False
+            new_render = True
+            
+            if key_name == "1":
+                # meta index only, no render
+                if manager.run_level > 2:
+                    unload_sprites = True
+                if manager.run_level > 1:
+                    unload_data = True
+                new_render = False
+            if key_name == "2":
+                # data loaded but no sprites; no render
+                if manager.run_level > 2:
+                    unload_sprites = True
+                if manager.run_level < 2:
+                    load_data = True
+                new_render = False
+            if key_name == "3":
+                # everything is loaded; render
+                if manager.run_level < 2:
+                    load_data = True
+                if manager.run_level < 3:
+                    load_sprites = True
+                new_render = True
+
+            for tileset in manager.tile_sets:
+                if load_data:
+                    manager.load_tileset_data(tileset)
+                if load_sprites:
+                    manager.load_tileset_sprite(tileset, 2)
+                if unload_sprites:
+                    manager.unload_tileset_sprite(tileset)
+                if unload_data:
+                    manager.unload_tileset_data(tileset)
+            
+            global do_render
+            do_render = new_render
+
+def render(manager, my_map):
+    screen.fill(black)
+    if do_render:
+        my_map.render(manager)
+    pygame.display.flip()    
 
 def main():
     manager = TileManager("./assets/index")
     manager.load_tileset_data("dungeon")
-    manager.load_tileset_sprite("dungeon")
-    print (manager.tile_sets)
+    manager.load_tileset_sprite("dungeon", 2)
+
+    my_map = Map("dungeon", (32, 32))
+    my_map.load_data("map")
+
+    while (True):
+        start = time.time()
+        process_input(manager)
+        render(manager, my_map)
+        time.sleep(start + frame_time - time.time())
 
 if __name__ == "__main__":
     main()
